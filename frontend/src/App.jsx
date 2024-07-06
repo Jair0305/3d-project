1e6638200940b8d73b042123ac640ec7f6d8dc93
Jair0305 <chamaco_03@hotmail.es>
2024-07-06 11:37:51 -0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Experience} from "./components/Experience.jsx";
+import {Canvas} from "@react-three/fiber";
+
+function App() {
+
+    return (
+        <Canvas>
+            <Experience/>
+        </Canvas>
+    )
+}
+
+export default App
